f749fc53d958296fb807ebe2ec3863712db8ace8
1taeho93 <th930702@gmail.com>
2021-05-21 17:22:39 +0900

    f

--- app_express.js
+++ app_express.js
@@ -2,18 +2,29 @@ var express = require('express');
 var fs = require('fs');
 var app = express();
 
-app.get('/', function (req, res) {
-  fs.readFile('index.html', function (error, data) {
-    res.writeHead(200, { 'Content-Type': 'text/html'});
-    res.end(data);
-  });
-});
-app.get('/main', function (req, res) {
-  fs.readFile('main.html', function (error, data){
-    res.writeHead(200, { 'Content-Type': 'text/html'});
-    res.end(data);
-  });
-});
-app.listen(3303, function(){
+app.use(express.bodyParser());
+app.listen(3303, "localhost", function() {
   console.log('Server Start .');
+});
+
+
+app.get('/', function (req, res) {
+  fs.readFile('mainfile.html', function (error, data) {
+    res.writeHead(200, { 'Content-Type': 'text/html'});
+    res.end(data, function (error){
+      console.log(error);
+    });
+  });
+});
+app.post('/upload', function (req, res){
+  fs.readFile(req.files.videofile.path, function (error, data) {
+    var filePath = __dirname + "\\uploads\\" + req.file.videofile.name;
+    fs.writeFile(filePath, data, function(error){
+      if(error){
+        throw err;
+      } else {
+        res.redirect("back");
+      }
+    });
+  });
 });
