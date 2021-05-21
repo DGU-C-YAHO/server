78f7f29add48697b7f47f3e25396428086bba242
1taeho93 <th930702@gmail.com>
2021-05-21 18:08:11 +0900

    no....

--- app_express.js
+++ app_express.js
@@ -2,23 +2,26 @@ var express = require('express');
 var fs = require('fs');
 var app = express();
 
-app.use(express.bodyParser());
+//app.use(express.bodyParser());
+var bodyParser = require('body-parser');
+app.use(bodyParser());
 app.listen(3303, "localhost", function() {
   console.log('Server Start .');
 });
 
 
 app.get('/', function (req, res) {
-  fs.readFile('mainfile.html', function (error, data) {
+  fs.readFile('fileupload.html', function (error, data) {
     res.writeHead(200, { 'Content-Type': 'text/html'});
     res.end(data, function (error){
       console.log(error);
     });
   });
 });
+
 app.post('/upload', function (req, res){
-  fs.readFile(req.files.videofile.path, function (error, data) {
-    var filePath = __dirname + "\\uploads\\" + req.file.videofile.name;
+  fs.readFile(req.files.uploadFile.path, function (error, data) {
+    var filePath = __dirname + "\\uploads\\" + req.file.uploadFile.name;
     fs.writeFile(filePath, data, function(error){
       if(error){
         throw err;
