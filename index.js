c60aaa77801a918db6e42214889ce683de3b719f
1taeho93 <th930702@gmail.com>
2021-05-21 16:59:02 +0900

    asdf

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
-const express = require('express');
-const router = express.Router();
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-const multer = require('multer');
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, "uploads/");
-  },
-  filename: (req, file, callback) => {
-    callback(null, file.originalname);
-  }
-});
-const uploader = multer({storage:storage});
-
-//기본 GET 페이지
-router.get('/', (req, res, next) => {
-  res.render('mainchoice.html')
+server.listen(8011, () => {
+  console.log('Start the server using the port 8011');
 });
 
-//업로드 페이지
-router.post('mainfile.html', uploader.single('videofile'), (req, res, next) =>{
-  console.log(req.file);
-  console.log('파일 업로드')
-  res.redirect('/');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-module.exports = router;
+io.on('connection', (socket) => {
+  //server가 client쪽으로 보내는 이벤트
+  socket.emit('hello', {hello: 'world'});
+  //client 에서 보내는 이벤트를 처리
+  socket.on('first', function (data){
+    console.log(data);
+  });
+});
