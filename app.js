e846c8e42dbd691b992e3c332a9019f6dd74b05e
1taeho93 <th930702@gmail.com>
2021-06-03 17:13:21 +0900

    css 적용 완료

--- app.js
+++ app.js
@@ -60,22 +60,9 @@ app.get('/error', function (req, res){
     });
   });
 });
-/*
-app.post('/upload', function (req, res){
-  fs.readFile(req.files.uploadFile.path, function (error, data) {
-    var filePath = __dirname + "\\uploads\\" + req.file.uploadFile.name;
-    fs.writeFile(filePath, data, function(error){
-      if(error){
-        throw err;
-      } else {
-        res.redirect("back");
-      }
-    });
-  });
-});
-*/
-var multer = require('multer');
 
+var multer = require('multer');
+//파일 업로드
 var upload = multer({
   storage: multer.diskStorage({
     destination(req, file, done) {
@@ -87,21 +74,14 @@ var upload = multer({
     },
   }),
 });
-/*app.get('/upload', function (req, res){
-  fs.readFile('loading.html', function (error, data){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(data,function (error){
-      console.log(error);
-    });
-  });
-});*/
+
 app.get('/upload', (req, res) => {
   res.sendFile(path.join(__dirname, 'loading'));
 });
 app.post('/upload',
   upload.fields([{ name:'uploadFile'}]),
   (req, res) => {
-    fs.readFile('loading.html', function (error, data){
+    fs.readFile('loading.html', function (error, data){ //업로드 후 로딩페이지로 이동
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.end(data,function (error){
         console.log(error);
